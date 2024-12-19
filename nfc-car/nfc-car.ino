99080d8b6a0076eb5e3d2703486684e26758487c
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-12-19 14:47:54 -0600

    Defined logic to read out NFC tag's ID and determine whether it was a short or long hold near the reader

--- nfc-car/nfc-car.ino
+++ nfc-car/nfc-car.ino
@@ -6,10 +6,15 @@ int unlockPin = 6;
 int lockPin = 7;
 int nfcRstPin = 8;
 int nfcSSPin = 9;
+String tag = "";
 
 Servo unlockServo;
 Servo lockServo;
 MFRC522 nfcReader(nfcSSPin, nfcRstPin);
+
+bool tagPresent = false;
+long tagStart = 0;
+long tagStop = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -25,9 +30,48 @@ void setup() {
 }
 
 void loop() {
-  delay(5000);
+  scanTags();
 }
 
+void scanTags() {
+  if (nfcReader.PICC_IsNewCardPresent() && nfcReader.PICC_ReadCardSerial()) {
+    if (!tagPresent) {
+      tagPresent = true;
+      tagStart = millis();
+    }
+
+    tag = "";
+
+    for (byte i = 0; i < nfcReader.uid.size; i++) {
+      if (nfcReader.uid.uidByte[i] < 0x10) { tag += "0"; }
+      tag += String(nfcReader.uid.uidByte[i], HEX);
+    }
+
+    Serial.println(tag);
+  } else if (tagPresent) {
+    tagPresent = false;
+    tagStop = millis();
+
+    long duration = tagStop - tagStart;
+
+    if (isValidTag(tag)) {
+      if (duration < 1000) {
+        unlock();
+      } else {
+        lock();
+      }    
+    }
+
+    nfcReader.PICC_HaltA();
+    tag = "";
+  }
+}
+
+// Check for actual validity of card/tag later
+// defining a config.h for all the tags ids & valid strings for the eventual webserver
+bool isValidTag(String tag) {
+  return true;
+}
 
 void lock() {
   lockServo.write(90);
